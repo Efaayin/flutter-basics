8a6926d65f9b443ed031319871abde1e8eaecf76
Ifeanyi <alichiifeanyi@yahoo.com>
2021-04-30 13:49:59 +0100

    more updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,87 @@
 import 'package:flutter/material.dart';
 
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return _MyAppState();
   }
-
 }
 
 class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What is your favorite color?',
+      'answers': [
+        {'Text': 'Black', 'score': 10},
+        {'Text': 'Red', 'score': 5},
+        {'Text': 'Green', 'score': 3},
+        {'Text': 'White', 'score': 1},
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        {'Text': 'Lion', 'score': 5},
+        {'Text': 'Cat', 'score': 1},
+        {'Text': 'Cow', 'score': 3},
+        {'Text': 'Dog', 'score': 9},
+      ],
+    },
+    {
+      'questionText': 'Who\'s your favorite instructor?',
+      'answers': [
+        {'Text': 'Dev Ed', 'score': 1},
+        {'Text': 'Tech Lead', 'score': 1},
+        {'Text': 'Brad', 'score': 1},
+        {'Text': 'Max', 'score': 1},
+      ],
+    },
+  ];
 
   var _questionIndex = 0;
+  var _totalScore = 0;
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
+
+    if (_questionIndex < _questions.length) {
+      print('We have more questions');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': 'What is your favorite color?', 
-        'answers': ['Black', 'Red', 'Green', 'White'],
-      },
-
-      {
-        'questionText': "What's your favorite animal?", 
-        'answers': ['Lion', 'Cat', 'Cow', 'Dog'],
-      },
-
-      {
-        'questionText': "Who's your favorite instructor?", 
-        'answers': ['Dev Ed', 'Tech Lead', 'Brad', 'Max'],
-      },
-    ];
-
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
         title: Text('My First App'),
       ),
-      body: Column(
-        children: [
-          Question(
-            questions[_questionIndex]['questionText']
-          ),
-          ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
-            return Answer(_answerQuestion ,answer);
-          }).toList()
-        //   Answer(_answerQuestion),
-        //   Answer(_answerQuestion),
-        //   Answer(_answerQuestion),
-        ],
-      ),
+      body: _questionIndex < _questions.length
+          ? Quiz(
+              answerQuestion: _answerQuestion,
+              questionIndex: _questionIndex,
+              questions: _questions,
+            )
+          : Result(_totalScore, _resetQuiz),
     ));
   }
 }
